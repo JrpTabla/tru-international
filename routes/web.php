3de38f9ca6929b6b69ca66d95ecb50540917094b
JrpTabla <james.ray.p.tabla@mail.com>
2024-02-13 15:47:37 +0800

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('pages/homepage', ['title' => 'TradersUnited']);
+});
+
+Route::get('/risk-protection-program', function () {
+    return view('pages/risk-protection-program', ['title' => 'TradersUnited | Risk Protection Program']);
+});
+
+Route::get('/unity-gains', function () {
+    return view('pages/unity-gains', ['title' => 'TradersUnited | Unity Gains']);
+});
